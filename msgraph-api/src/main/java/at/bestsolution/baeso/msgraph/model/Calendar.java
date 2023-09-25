636320ffd57d0b2fe3b2af117adc243069517894
Tom Schindl <tom.schindl@bestsolution.at>
2023-09-25 15:36:10 +0200

    map calendar properties

--- msgraph-api/src/main/java/at/bestsolution/baeso/msgraph/model/Calendar.java
+++ msgraph-api/src/main/java/at/bestsolution/baeso/msgraph/model/Calendar.java
@@ -3,20 +3,190 @@ package at.bestsolution.baeso.msgraph.model;
 import at.bestsolution.baeso.msgraph.base.ID;
 
 public interface Calendar {
+    public enum Color {
+        AUTO("auto"),
+        LIGHT_BLUE("lightBlue"),
+        LIGHT_GREEN("lightGreen"),
+        LIGHT_ORANGE("lightOrange"),
+        LIGHT_GRAY("lightGray"),
+        LIGHT_YELLOW("lightYellow"),
+        LIGHT_TEAL("lightTeal"),
+        LIGHT_PINK("lightPink"),
+        LIGHT_BROWN("lightBrown"),
+        LIGHT_RED("lightRed"),
+        MAX_COLOR("maxColor");
+
+        private final String value;
+
+        Color(String value) {
+            this.value = value;
+        }
+
+        public String value() {
+            return this.value;
+        }
+
+        public static Color of(String value) {
+            return switch (value) {
+                case "auto" -> AUTO;
+                case "lightBlue" -> LIGHT_BLUE;
+                case "lightGreen" -> LIGHT_GREEN;
+                case "lightOrange" -> LIGHT_ORANGE;
+                case "lightGray" -> LIGHT_GRAY;
+                case "lightYellow" -> LIGHT_YELLOW;
+                case "lightTeal" -> LIGHT_TEAL;
+                case "lightPink" -> LIGHT_PINK;
+                case "lightBrown" -> LIGHT_BROWN;
+                case "lightRed" -> LIGHT_RED;
+                case "maxColor" -> MAX_COLOR;
+                default -> throw new IllegalArgumentException(String.format("Unknown value '%s'", value));
+            };
+        }
+    }
+
+    public enum OnlineMeetingProviderType {
+        UNKNOWN("unknown"),
+        SKYPE_FOR_BUSINESS("skypeForBusiness"),
+        SKYPE_FOR_CONSUMER("skypeForConsumer"),
+        TEAMS_FOR_BUSINESS("teamsForBusiness");
+
+        private final String value;
+
+        OnlineMeetingProviderType(String value) {
+            this.value = value;
+        }
+
+        public String value() {
+            return this.value;
+        }
+
+        public static OnlineMeetingProviderType of(String value) {
+            return switch (value) {
+                case "unknown" -> UNKNOWN;
+                case "skypeForBusiness" -> SKYPE_FOR_BUSINESS;
+                case "skypeForConsumer" -> SKYPE_FOR_CONSUMER;
+                case "teamsForBusiness" -> TEAMS_FOR_BUSINESS;
+                default -> throw new IllegalArgumentException(String.format("Unknown value '%s'", value));
+            };
+        }
+    }
+
+    /**
+     * The calendar's unique identifier. Read-only.
+     * 
+     * @return value
+     */
     public ID<Calendar> id();
-    // allowedOnlineMeetingProviders
+
+    /**
+     * The calendar name.
+     * 
+     * @return value
+     */
     public String name();
+
+    /**
+     * <code>true</code> if the user can write to the calendar, <code>false</code>
+     * otherwise. This property is <code>true</code>‚ for the user who created the
+     * calendar. This property is also <code>true</code> for a user who has been
+     * shared a calendar and granted write access.
+     * 
+     * @return
+     */
     public boolean canEdit();
+
+    /**
+     * <code>true</code> if the user has the permission to share the calendar,
+     * <code>false</code> otherwise. Only the user who created the calendar can
+     * share it.
+     * 
+     * @return value
+     */
     public boolean canShare();
-    // canViewPrivateItems
-    // changeKey
-    // color
-    // defaultOnlineMeetingProvider
-    // hexColor
-    // isDefaultCalendar
-    // isRemovable
-    // isTallyingResponses
-    // owner
+
+    /**
+     * <code>true</code> if the user can read calendar items that have been marked
+     * private, <code>false</code> otherwise.
+     * 
+     * @return value
+     */
+    boolean canViewPrivateItems();
+
+    /**
+     * Identifies the version of the calendar object. Every time the calendar is
+     * changed, changeKey changes as well. This allows Exchange to apply changes to
+     * the correct version of the object. Read-only.
+     * 
+     * @return value
+     */
+    String changeKey();
+
+    /**
+     * Specifies the color theme to distinguish the calendar from other calendars in
+     * a UI. The property values are: <code>auto</code>, <code>lightBlue</code>,
+     * <code>lightGreen</code>, <code>lightOrange</code>, <code>lightGray</code>,
+     * <code>lightYellow</code>, <code>lightTeal</code>, <code>lightPink</code>,
+     * <code>lightBrown</code>, <code>lightRed</code>, <code>maxColor</code>.
+     * 
+     * @return value
+     */
+    Color color();
+
+    /**
+     * The default online meeting provider for meetings sent from this calendar.
+     * Possible values are: <code>unknown</code>, <code>skypeForBusiness</code>,
+     * <code>skypeForConsumer</code>, <code>teamsForBusiness</code>.
+     * 
+     * @return value
+     */
+    OnlineMeetingProviderType defaultOnlineMeetingProvider();
+
+    /**
+     * The calendar color, expressed in a hex color code of three hexadecimal
+     * values, each ranging from 00 to FF and representing the red, green, or blue
+     * components of the color in the RGB color space. If the user has never
+     * explicitly set a color for the calendar, this property is empty. Read-only.
+     * 
+     * @return value
+     */
+    String hexColor();
+
+    /**
+     * <code>true</code> if this is the default calendar where new events are
+     * created by default, <code>false</code> otherwise.
+     * 
+     * @return value
+     */
+    boolean isDefaultCalendar();
+
+    /**
+     * Indicates whether this user calendar can be deleted from the user mailbox.
+     * 
+     * @return value
+     */
+    boolean isRemovable();
+
+    /**
+     * Indicates whether this user calendar supports tracking of meeting responses.
+     * Only meeting invites sent from users' primary calendars support tracking of
+     * meeting responses.
+     * 
+     * @return value
+     */
+    boolean isTallyingResponses();
+
+    /**
+     * If set, this represents the user who created or added the calendar. For a
+     * calendar that the user created or added, the <strong>owner</strong> property
+     * is set to the user. For a calendar shared with the user, the
+     * <strong>owner</strong> property is set to the person who shared that calendar
+     * with the user.
+     * 
+     * @return value
+     */
+    EmailAddress owner();
+
+    // allowedOnlineMeetingProviders
 
     public interface Builder {
         public Calendar build();
--- msgraph-impl/src/main/java/at/bestsolution/baeso/msgraph/impl/model/CalendarImpl.java
+++ msgraph-impl/src/main/java/at/bestsolution/baeso/msgraph/impl/model/CalendarImpl.java
@@ -1,7 +1,9 @@
 package at.bestsolution.baeso.msgraph.impl.model;
 
 import at.bestsolution.baeso.msgraph.base.ID;
+import at.bestsolution.baeso.msgraph.impl.utils.JsonUtils;
 import at.bestsolution.baeso.msgraph.model.Calendar;
+import at.bestsolution.baeso.msgraph.model.EmailAddress;
 
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -32,6 +34,56 @@ public class CalendarImpl implements Calendar {
     @Override
     public boolean canShare() {
         return object.getBoolean("canShare", true);
+    }
+
+    @Override
+    public boolean canViewPrivateItems() {
+        return object.getBoolean("canViewPrivateItems");
+    }
+
+    @Override
+    public String changeKey() {
+        return object.getString("changeKey");
+    }
+
+    @Override
+    public Color color() {
+        return Color.of(object.getString("color"));
+    }
+
+    @Override
+    public OnlineMeetingProviderType defaultOnlineMeetingProvider() {
+        return OnlineMeetingProviderType.of(object.getString("defaultOnlineMeetingProvider"));
+    }
+
+    @Override
+    public String hexColor() {
+        return object.getString("hexColor");
+    }
+
+    @Override
+    public boolean isDefaultCalendar() {
+        return object.getBoolean("isDefaultCalendar");
+    }
+
+    @Override
+    public boolean isRemovable() {
+        return object.getBoolean("isRemovable");
+    }
+
+    @Override
+    public boolean isTallyingResponses() {
+        return object.getBoolean("isTallyingResponses");
+    }
+
+    @Override
+    public EmailAddress owner() {
+        return new EmailAddressImpl(object.getJsonObject("owner"));
+    }
+
+    @Override
+    public String toString() {
+        return JsonUtils.stringify(object, true);
     }
 
     public static class CalendarBuilderImpl implements Calendar.Builder {
